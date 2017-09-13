532eefb63b3bd88f0d43b7eb342609616581f78b
EFanZh <efanzh@gmail.com>
2017-09-13 21:13:54 +0800

    Add EOPL exercise 2.22

---
+++ exercise-2.22.rkt
@@ -0,0 +1,32 @@
+#lang eopl
+
+;; Exercise 2.22 [★] Using define-datatype, implement the stack data type of exercise 2.4.
+
+(define scheme-value?
+  (lambda (value)
+    #t))
+
+(define-datatype stack-type stack?
+  (empty-stack)
+  (push [saved-stack stack?]
+        [val scheme-value?]))
+
+(define pop
+  (lambda (stack)
+    (cases stack-type stack
+      [empty-stack () (eopl:error 'pop "Can not pop an empty stack.")]
+      [push (saved-stack val) saved-stack])))
+
+(define top
+  (lambda (stack)
+    (cases stack-type stack
+      [empty-stack () (eopl:error 'pop "Can not top an empty stack.")]
+      [push (saved-stack val) val])))
+
+(define empty-stack?
+  (lambda (stack)
+    (cases stack-type stack
+      [empty-stack () #t]
+      [push (saved-stack val) #f])))
+
+(provide empty-stack push pop top empty-stack?)
