7f5aade8ce78bf53916361081e8cedf2c97bb852
EFanZh <efanzh@gmail.com>
2017-12-24 11:15:10 +0800

    Add two more test cases

--- exercise-3.42-test.rkt
+++ exercise-3.42-test.rkt
@@ -10,3 +10,16 @@
                                 -(z, x)
                      9)")
               (num-val 6))
+
+(check-equal? (run "(let x = 6
+                     in let y = 4
+                        in proc ()
+                             -(x, y))")
+              (num-val 2))
+
+(check-equal? (run "((proc (x, y)
+                        proc ()
+                          -(x, y)
+                      7
+                      3))")
+              (num-val 4))
