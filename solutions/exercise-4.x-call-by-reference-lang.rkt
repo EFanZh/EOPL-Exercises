48cfff05918cf529651f8128768aeb921abfd5bd
EFanZh <efanzh@gmail.com>
2018-02-07 13:09:36 +0800

    Add exercise 4.36

--- solutions/exercise-4.x-call-by-reference-lang.rkt
+++ solutions/exercise-4.x-call-by-reference-lang.rkt
@@ -31,7 +31,10 @@
     [expression ("left" "(" expression ")") left-exp]
     [expression ("setleft" expression "=" expression) setleft-exp]
     [expression ("right" "(" expression ")") right-exp]
-    [expression ("setright" expression "=" expression) setright-exp]))
+    [expression ("setright" expression "=" expression) setright-exp]
+    [expression ("newarray" "(" expression "," expression ")") newarray-exp]
+    [expression ("arrayref" "(" expression "," expression ")") arrayref-exp]
+    [expression ("arrayset" "(" expression "," expression "," expression ")") arrayset-exp]))
 
 (sllgen:make-define-datatypes the-lexical-spec the-grammar)
 
@@ -74,11 +77,40 @@
   (lambda (p val)
     (setref! (+ 1 p) val)))
 
+(define-datatype array array?
+  [an-array [ref reference?]
+            [length (lambda (x)
+                      (and (integer? x)
+                           (positive? x)))]])
+
+(define (make-array length value)
+  (if (positive? length)
+      (let ([result (newref value)])
+        (let loop ([i 1])
+          (if (< i length)
+              (begin (newref value)
+                     (loop (+ i 1)))
+              (an-array result length))))
+      (eopl:error 'make-array "The array length must be greater than 0.")))
+
+(define (array-ref array1 index)
+  (cases array array1
+    [an-array (ref length) (if (< index length)
+                               (+ ref index)
+                               (eopl:error 'array-ref "Array index out of bound."))]))
+
+(define (set-array! array1 index value)
+  (cases array array1
+    [an-array (ref length) (if (< index length)
+                               (setref! (+ ref index) value)
+                               (eopl:error 'array-ref "Array index out of bound."))]))
+
 (define-datatype expval expval?
   [num-val [value number?]]
   [bool-val [boolean boolean?]]
   [proc-val [proc proc?]]
-  [mutpair-val [p mutpair?]])
+  [mutpair-val [p mutpair?]]
+  [array-val [array array?]])
 
 (define expval-extractor-error
   (lambda (variant value)
@@ -107,6 +139,12 @@
     (cases expval v
       [mutpair-val (ref) ref]
       [else (expval-extractor-error 'mutable-pair v)])))
+
+(define expval->array
+  (lambda (v)
+    (cases expval v
+      [array-val (array) array]
+      [else (expval-extractor-error 'array v)])))
 
 ;; Environments.
 
@@ -197,6 +235,9 @@
   (lambda (exp env)
     (cases expression exp
       [var-exp (var) (apply-env env var)]
+      [arrayref-exp (exp1 exp2) (let ([arr (expval->array (value-of exp1 env))]
+                                      [index (expval->num (value-of exp2 env))])
+                                  (array-ref arr index))]
       [else (newref (value-of exp env))])))
 
 (define value-of-operand*
@@ -276,7 +317,18 @@
                                       [v2 (value-of exp2 env)])
                                   (let ([p (expval->mutpair v1)])
                                     (begin (setright p v2)
-                                           (num-val 83))))])))
+                                           (num-val 83))))]
+      [newarray-exp (exp1 exp2) (let ([length (expval->num (value-of exp1 env))]
+                                      [value (value-of exp2 env)])
+                                  (array-val (make-array length value)))]
+      [arrayref-exp (exp1 exp2) (let ([arr (expval->array (value-of exp1 env))]
+                                      [index (expval->num (value-of exp2 env))])
+                                  (deref (array-ref arr index)))]
+      [arrayset-exp (exp1 exp2 exp3) (let ([arr (expval->array (value-of exp1 env))]
+                                           [index (expval->num (value-of exp2 env))]
+                                           [value (value-of exp3 env)])
+                                       (set-array! arr index value)
+                                       (num-val 73))])))
 
 (define value-of-program
   (lambda (pgm)
---
+++ tests/exercise-4.36-test.rkt
@@ -0,0 +1,150 @@
+#lang racket
+
+(require rackunit)
+(require "../solutions/exercise-4.x-call-by-reference-lang.rkt")
+
+(check-equal? (run "let a = newarray(2, -99)
+                    in let p = proc (x)
+                                 let v = arrayref(x, 1)
+                                 in arrayset(x, 1, -(v, -1))
+                       in begin arrayset(a, 1, 0);
+                                (p a);
+                                (p a);
+                                arrayref(a,1)
+                          end")
+              (num-val 2))
+
+(check-equal? (run "arrayref(newarray(3, 2), 0)") (num-val 2))
+(check-equal? (run "arrayref(newarray(3, 2), 1)") (num-val 2))
+(check-equal? (run "arrayref(newarray(3, 2), 2)") (num-val 2))
+
+(check-equal? (run "let a = newarray(3, 2)
+                    in begin arrayset(a, 0, 4);
+                             arrayref(a, 0)
+                       end")
+              (num-val 4))
+
+(check-equal? (run "let a = newarray(3, 2)
+                    in begin arrayset(a, 1, 4);
+                             arrayref(a, 0)
+                       end")
+              (num-val 2))
+
+(check-equal? (run "let a = newarray(3, 2)
+                    in begin arrayset(a, 2, 4);
+                             arrayref(a, 0)
+                       end")
+              (num-val 2))
+
+(check-equal? (run "let a = newarray(3, 2)
+                    in begin arrayset(a, 0, 4);
+                             arrayref(a, 1)
+                       end")
+              (num-val 2))
+
+(check-equal? (run "let a = newarray(3, 2)
+                    in begin arrayset(a, 1, 4);
+                             arrayref(a, 1)
+                       end")
+              (num-val 4))
+
+(check-equal? (run "let a = newarray(3, 2)
+                    in begin arrayset(a, 2, 4);
+                             arrayref(a, 1)
+                       end")
+              (num-val 2))
+
+(check-equal? (run "let swap = proc (x)
+                                 proc (y)
+                                   let temp = x
+                                   in begin set x = y;
+                                            set y = temp
+                                      end
+                    in let a = newarray(4, 3)
+                       in let i = 1
+                          in let j = 2
+                             in begin arrayset(a, j, 5);
+                                      ((swap arrayref(a, i)) arrayref(a, j));
+                                      -(arrayref(a, 1), arrayref(a, 2))
+                                end")
+              (num-val 2))
+
+(check-equal? (run "let swap = proc (x)
+                                 proc (y)
+                                   let temp = x
+                                   in begin set x = y;
+                                            set y = temp
+                                      end
+                    in let a = newarray(4, 0)
+                       in let i = 1
+                          in let j = 2
+                             in begin arrayset(a, i, 3);
+                                      arrayset(a, j, 5);
+                                      ((swap arrayref(a, i)) arrayref(a, j));
+                                      -(arrayref(a, 1), arrayref(a, 2))
+                                end")
+              (num-val 2))
+
+(check-equal? (run "let swap = proc (x)
+                                 proc (y)
+                                   let temp = x
+                                   in begin set x = y;
+                                            set y = temp
+                                      end
+                    in let a = newarray(4, 0)
+                       in let i = 1
+                          in let j = 2
+                             in begin arrayset(a, i, 3);
+                                      arrayset(a, j, 5);
+                                      ((swap arrayref(a, arrayref(a, i))) arrayref(a, j));
+                                      arrayref(a, 0)
+                                end")
+              (num-val 0))
+
+(check-equal? (run "let swap = proc (x)
+                                 proc (y)
+                                   let temp = x
+                                   in begin set x = y;
+                                            set y = temp
+                                      end
+                    in let a = newarray(4, 0)
+                       in let i = 1
+                          in let j = 2
+                             in begin arrayset(a, i, 3);
+                                      arrayset(a, j, 5);
+                                      ((swap arrayref(a, arrayref(a, i))) arrayref(a, j));
+                                      arrayref(a, 1)
+                                end")
+              (num-val 3))
+
+(check-equal? (run "let swap = proc (x)
+                                 proc (y)
+                                   let temp = x
+                                   in begin set x = y;
+                                            set y = temp
+                                      end
+                    in let a = newarray(4, 0)
+                       in let i = 1
+                          in let j = 2
+                             in begin arrayset(a, i, 3);
+                                      arrayset(a, j, 5);
+                                      ((swap arrayref(a, arrayref(a, i))) arrayref(a, j));
+                                      arrayref(a, 2)
+                                end")
+              (num-val 0))
+
+(check-equal? (run "let swap = proc (x)
+                                 proc (y)
+                                   let temp = x
+                                   in begin set x = y;
+                                            set y = temp
+                                      end
+                    in let a = newarray(4, 0)
+                       in let i = 1
+                          in let j = 2
+                             in begin arrayset(a, i, 3);
+                                      arrayset(a, j, 5);
+                                      ((swap arrayref(a, arrayref(a, i))) arrayref(a, j));
+                                      arrayref(a, 3)
+                                end")
+              (num-val 5))
