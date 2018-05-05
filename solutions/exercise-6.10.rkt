3a43a929d633ad61838b2f72a5842cafb94868bf
EFanZh <efanzh@gmail.com>
2018-05-05 11:51:01 +0800

    Add exercise 6.10

---
+++ solutions/exercise-6.10.rkt
@@ -0,0 +1,20 @@
+#lang eopl
+
+;; Exercise 6.10 [★] For list-sum, formulate a succinct representation of the continuations, like the one for fact/k
+;; above.
+
+(define list-sum
+  (lambda (loi)
+    (if (null? loi)
+        0
+        (+ (car loi)
+           (list-sum (cdr loi))))))
+
+(define list-sum/k
+  (lambda (loi cont)
+    (if (null? loi)
+        cont
+        (list-sum (cdr loi)
+                  (+ cont (car loi))))))
+
+(provide list-sum list-sum/k)
---
+++ tests/exercise-6.10-test.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+(require rackunit)
+(require "../solutions/exercise-6.10.rkt")
+
+(define (list-sum-2 loi)
+  (list-sum/k loi 0))
+
+(for ([f (list list-sum list-sum-2)])
+  (check-eq? (f '()) 0)
+  (check-eq? (f '(0)) 0)
+  (check-eq? (f '(1)) 1)
+  (check-eq? (f '(2)) 2)
+  (check-eq? (f '(0 1)) 1)
+  (check-eq? (f '(0 1 2)) 3)
+  (check-eq? (f '(0 1 2 3)) 6)
+  (check-eq? (f '(0 1 2 3 4)) 10))
